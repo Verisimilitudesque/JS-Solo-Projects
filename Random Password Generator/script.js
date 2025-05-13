41c7d3790d93a21e7cfc101930ddb773e0d4b2b6
Jon <140838296+Verisimilitudesque@users.noreply.github.com>
2025-05-13 08:47:28 -0600

    Added styling, finalized javascript for generating password

--- Random Password Generator/script.js
+++ Random Password Generator/script.js
@@ -5,15 +5,32 @@ const symbols = "!@#$%^&*_-+=";
 
 // variable dom elements for stopring the password
 const passwordTxt = document.getElementById("ver-password");
-const passwordLngth = document.getElementById("ver-length");
-const passwordNmbrs = document.getElementById("ver-numbers");
+const length = document.getElementById("ver-length");
+const passwordNumbers = document.getElementById("ver-numbers");
 const passwordSymbols = document.getElementById("ver-symbols");
 const generatePsswd = document.getElementById("ver-generate");
 
-// function to generate password
+// function to listen for password
 generatePsswd.addEventListener("click", () => {
   let characters = alpha;
-  passwordNmbrs .checked ? (characters += numbers) : "";
-  passwordNmbrs .checked ? (characters += symbols) : "";
+  passwordNumbers.checked ? (characters += numbers) : "";
+  passwordSymbols.checked ? (characters += symbols) : "";
   passwordTxt.value = generatePassword(length.value, characters);
 });
+
+// function to generate password
+const generatePassword = (length, characters) => {
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return password;
+};
+//function to copy password
+const copyPassword = () => {
+  passwordTxt.select();
+  document.execCommand("copy");
+  alert("Password copied to clipboard!");
+}
